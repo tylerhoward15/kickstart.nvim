b8b8de4a38111dd377fdf2a3142d21e3ebc86bb9
Tyler Howard <tyler.howard@ipipeline.com>
2024-08-29 14:50:24 -0400

    Replace netrw with neotree

--- init.lua
+++ init.lua
@@ -996,5 +996,16 @@ require('lazy').setup({
   },
 })
 
+-- Disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Enable Neo-tree
+require('neo-tree').setup {
+  filesystem = {
+    hijack_netrw_behavior = 'open_default',
+  },
+}
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
