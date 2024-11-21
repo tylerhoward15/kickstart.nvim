9dbabff8e8d1c47167521c9f7f98fd88c3a5e03d
Tyler Howard <tyler.howard@ipipeline.com>
2024-11-21 14:25:25 -0500

    Revert "Add more languages to LSP and treesitter"
    This reverts commit f616346d5e7b26eeaaeed9be41168a21b49eaeb7.

--- init.lua
+++ init.lua
@@ -902,33 +902,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = {
-        'bash',
-        'c',
-        'diff',
-        'html',
-        'lua',
-        'luadoc',
-        'markdown',
-        'markdown_inline',
-        'query',
-        'vim',
-        'vimdoc',
-        'xml',
-        'javascript',
-        'java',
-        'json',
-        'go',
-        'groovy',
-        'dockerfile',
-        'css',
-        'kotlin',
-        'powershell',
-        'properties',
-        'python',
-        'sql',
-        'yaml',
-      },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -959,8 +933,8 @@ require('lazy').setup({
   --
   -- require 'kickstart.plugins.debug',
   require 'kickstart.plugins.indent_line',
-  require 'kickstart.plugins.lint',
-  require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
